72be39dab4d5a3c7e4d2ca53291a7a0be9fd39a5
taku1219 <acmiran189926@gmail.com>
2022-03-23 20:42:51 +0900

    [add] getメソッド仮

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+app.get('/api/v1/tasks', (req, res) => {
+  res.send('タスクを全て取得しました');
+});
+
 app.listen(PORT, console.log('サーバーが起動しました'));
