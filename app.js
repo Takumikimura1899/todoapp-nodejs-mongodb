39c4e20727f62a5ff3f72c56b6f5c56865259510
taku1219 <acmiran189926@gmail.com>
2022-03-23 20:49:35 +0900

    [add]deleteメソッド仮

--- app.js
+++ app.js
@@ -18,4 +18,8 @@ app.patch('/api/v1/tasks/:id', (req, res) => {
   res.send('ある特定のタスクを更新しました');
 });
 
+app.delete('/api/v1/tasks/:id', (req, res) => {
+  res.send('ある特定のタスクを削除しました');
+});
+
 app.listen(PORT, console.log('サーバーが起動しました'));
