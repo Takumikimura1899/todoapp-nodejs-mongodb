ac6d8de40b670084a42645751700bcb229835e57
taku1219 <acmiran189926@gmail.com>
2022-03-24 20:03:19 +0900

    [add]mongodbと接続

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 const taskRoute = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
 const PORT = 5000;
 
+// ルーティング設計
 app.use('/api/v1/tasks', taskRoute);
+
+// データベースと接続
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
 
 app.listen(PORT, console.log('サーバーが起動しました'));
