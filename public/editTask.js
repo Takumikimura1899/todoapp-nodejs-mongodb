3df177cb908720fa9283dde038c4aa92d06232e9
taku1219 <acmiran189926@gmail.com>
2022-03-26 09:36:15 +0900

    [add]完了・未完了のチェックによって表示が変わるように

--- public/editTask.js
+++ public/editTask.js
@@ -2,6 +2,7 @@ const taskIDDOM = document.querySelector('.task-edit-id');
 const taskNameDOM = document.querySelector('.task-edit-name');
 const editFormDOM = document.querySelector('.single-task-form');
 const formAlertDOM = document.querySelector('.form-alert');
+const taskCompletedDOM = document.querySelector('.task-edit-completed');
 
 const params = window.location.search;
 const id = new URLSearchParams(params).get('id');
@@ -15,6 +16,9 @@ const showTask = async () => {
     const { _id, completed, name } = task;
     taskIDDOM.textContent = _id;
     taskNameDOM.value = name;
+    if (completed) {
+      taskCompletedDOM.checked = true;
+    }
   } catch (err) {
     console.log(err);
   }
@@ -27,8 +31,10 @@ editFormDOM.addEventListener('submit', async (e) => {
   e.preventDefault();
   try {
     const taskName = taskNameDOM.value;
+    taskCompleted = taskCompletedDOM.checked;
     const { data: task } = await axios.patch(`/api/v1/tasks/${id}`, {
       name: taskName,
+      completed: taskCompleted,
     });
     formAlertDOM.display = 'block';
     formAlertDOM.textContent = '編集に成功しました';
